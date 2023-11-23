a9ab892591f2fc91825f913bfab334917f5b3be0
Jwilliamsbaxter <jwilliamsbaxter@gmail.com>
2023-11-23 15:04:16 -0500

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   todo/.editorconfig
    	new file:   todo/.gitignore
    	new file:   todo/.vscode/extensions.json
    	new file:   todo/.vscode/launch.json
    	new file:   todo/.vscode/tasks.json
    	new file:   todo/README.md
    	new file:   todo/angular.json
    	new file:   todo/package-lock.json
    	new file:   todo/package.json
    	new file:   todo/src/app/app.component.css
    	new file:   todo/src/app/app.component.html
    	new file:   todo/src/app/app.component.spec.ts
    	new file:   todo/src/app/app.component.ts
    	new file:   todo/src/app/app.module.ts
    	new file:   todo/src/app/item.ts
    	new file:   todo/src/app/item/item.component.css
    	new file:   todo/src/app/item/item.component.html
    	new file:   todo/src/app/item/item.component.spec.ts
    	new file:   todo/src/app/item/item.component.ts
    	new file:   todo/src/assets/.gitkeep
    	new file:   todo/src/favicon.ico
    	new file:   todo/src/index.html
    	new file:   todo/src/main.ts
    	new file:   todo/src/styles.css
    	new file:   todo/tsconfig.app.json
    	new file:   todo/tsconfig.json
    	new file:   todo/tsconfig.spec.json
    
    commit

---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo';
+  filter: string = "all"; // Change the type to string
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  removeItem(item: any) { // Change the parameter type to any or specify the correct type
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+}
